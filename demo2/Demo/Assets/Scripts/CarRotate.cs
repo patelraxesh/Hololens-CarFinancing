6cd0b01da7484e3bb0e3b526f19e8ab90e9d6509
patelraxesh <raxeshp1991@gmail.com>
2017-07-04 18:21:06 -0400

    added files

---
+++ demo2/Demo/Assets/Scripts/CarRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarRotate : MonoBehaviour {
+
+	int t_corolla = 18680;
+	int t_prius = 21000;
+	int t_camry = 23500;
+
+	// Use this for initialization
+	void Start () {
+		print ("This is car shop");
+		print ("Toyota camry's price is " + t_camry);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			print ("Up arrow");
+	}
+}
+}
